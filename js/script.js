ded515207ed4b0b66d2eeb3a751e8454aaf15585
Person 2 <person1@gmail.com>
2019-07-17 11:22:20 +0530

    changes in css and js and html

--- js/script.js
+++ js/script.js
@@ -27,25 +27,16 @@ $(function() {
   });
 });
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-    document.getElementById("toTop").style.display = "block";
-  } else {
-    document.getElementById("toTop").style.display = "none";
-  }
-}
+// go to top button
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
 
-$(document).ready(function(){
-       $(".fa fa-navicon").click(function(){
-           $("navi_wrap").slideToggle();
-       });
-   });
+var goToTop = document.getElementById('goToTop');
+
+    document.addEventListener('scroll' , function() {
+      if(scrollY > 200) {
+        goToTop.classList.add('active');
+      } else {
+        goToTop.classList.remove('active');
+      }
+    })
